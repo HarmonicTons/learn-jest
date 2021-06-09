a899255b28fe4226d06ece87ad6fcdbfba3c6b35
HarmonicTons <roncin.thomas.l.l@gmail.com>
2021-06-09 12:44:56 +0000

    init bowling TDD

--- src/fetchCrapaud.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-
-const fetchCrapaud = async () => {
-  const res = await axios.get("https://crapaud-provider.com");
-  return res.data;
-};
-
-module.exports = { fetchCrapaud };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-const { fetchCrapaud } = require("./fetchCrapaud");
-const { tranformeCrapaudEnPrince } = require("./tranformeCrapaudEnPrince");
+class Game {}
 
-const makeAPrince = async () => {
-  const crapaud = await fetchCrapaud();
-  const prince = tranformeCrapaudEnPrince(crapaud);
-  // store prince
-  return prince;
-};
-
-module.exports = { makeAPrince };
+module.exports = {Game};
